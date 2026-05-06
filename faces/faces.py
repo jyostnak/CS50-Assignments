d726316b7310e5666369e98212263ca5de183ea1
jyostnak <jyostnakondepi@gmail.com>
2026-05-06 11:18:28 +0000

    Wed, May 6, 2026, 4:48 PM +05:30

--- faces/faces.py
+++ faces/faces.py
@@ -1,7 +1,7 @@
 statement = input("Enter your statement:")
 def convert(sentance):
-    smile_converted = sentance.replace(":)", "🙂")
-    frown_converted = sentance.replace(":(","🙁")
+    sentance = sentance.replace(":)", "🙂")
+    sentance = sentance.replace(":(","🙁")
     return sentance
 print(convert(statement))
 
