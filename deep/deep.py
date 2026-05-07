676df135851db23bb12de6496ec912cd48dedeb9
jyostnak <jyostnakondepi@gmail.com>
2026-05-07 13:36:30 +0000

    Thu, May 7, 2026, 7:06 PM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,2 +1,10 @@
 a=input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+if(a=="42"):
+    print("Yes")
+elif(a=="forty-two"):
+    print("Yes")
+elif(a=="forty two"):
+    print("Yes")
+else:
+    print("no")
 
