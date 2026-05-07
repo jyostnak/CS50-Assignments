0dfd5f73450360f5f66ebc21796e225d677171e6
jyostnak <jyostnakondepi@gmail.com>
2026-05-07 13:41:12 +0000

    Thu, May 7, 2026, 7:11 PM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,9 +1,5 @@
 a=input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
-if(a=="42"):
-    print("Yes")
-elif(a=="forty-two"):
-    print("Yes")
-elif(a=="forty two"):
+if a in ["42","forty two","forty-two"]:
     print("Yes")
 else:
     print("no")
