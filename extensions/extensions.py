3fbced747f5c76c96b2ae80eceb1d66dbc4cd8b4
jyostnak <jyostnakondepi@gmail.com>
2026-05-08 09:45:28 +0000

    Fri, May 8, 2026, 3:15 PM +05:30

---
+++ extensions/extensions.py
@@ -0,0 +1,22 @@
+#jpg,jpeg,png,gif-->image
+#pdf,zip-->app
+#txt-->text/plain
+name = input('File Name:')
+name=name.strip().lower()
+name=name.replace('.',' ')
+name=name.split()
+image = ['gif','jpg','jpeg','png']
+app = ['pdf','zip']
+if set(name) & set(image):
+    b=list(set(name) & set(image))
+    b=''.join(b)
+    print("image/"+b)
+elif set(name) & set(app):
+    b=list(set(name) & set(app))
+    b=''.join(b)
+    print("application/"+b)
+elif 'txt' in name:
+    print('text/plain')
+else:
+    print("application/octet-stream")
+
