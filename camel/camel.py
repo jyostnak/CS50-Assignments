9807f01ba3ef5a9492fcb6122139c1501c517077
jyostnak <jyostnakondepi@gmail.com>
2026-05-10 15:11:20 +0000

    Sun, May 10, 2026, 8:41 PM +05:30

--- camel/camel.py
+++ camel/camel.py
@@ -1,2 +1,14 @@
-camelFont = input("camelCase:")
-for 
+camelFont = input('camelCase:')
+camelList = []
+for char in camelFont:
+    camelList.append(char)
+i = 0
+while i<len(camelList):
+    if 'A'<=camelList[i]<='Z':
+        camelList.insert(i,'_')
+        i+=1
+    i+=1
+snake_case = ''.join(camelList)
+snake_case = snake_case.lower()
+print(snake_case)
+
