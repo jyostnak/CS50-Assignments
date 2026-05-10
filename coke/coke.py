b707950bbeb7b9d5e518484c195c589c25c8d831
jyostnak <jyostnakondepi@gmail.com>
2026-05-10 15:48:51 +0000

    Sun, May 10, 2026, 9:18 PM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -1,9 +1,14 @@
-Amount = 50
-while True:
-    if Amount>0:
-        print('Amount due =',Amount)
-        given = int(input('Insert coin = '))
-        Amount -= given
-    else:
-        print('Change owed =', -Amount)
-        break
+amount_due = 50
+
+while amount_due > 0:
+
+    print(f"Amount Due: {amount_due}")
+
+    coin = int(input("Insert Coin: "))
+
+    if coin in [25, 10, 5]:
+        amount_due -= coin
+
+change_owed = abs(amount_due)
+
+print(f"Change Owed: {change_owed}")
