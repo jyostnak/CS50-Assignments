7796660d41edef632f07ce7646b0f85b94f5f26e
jyostnak <jyostnakondepi@gmail.com>
2026-05-12 12:09:28 +0000

    Tue, May 12, 2026, 5:39 PM +05:30

---
+++ fuel/fuel.py
@@ -0,0 +1,31 @@
+def convert(n):
+    lst = []
+    for ch in n:
+        lst.append(ch)
+    for i in range(len(lst)):
+        if lst[i] == '/':
+            before = int(''.join(lst[:i]))
+            after = int(''.join(lst[i+1:]))
+            result = (before/after)*100
+            if result<0 or result>100:
+                raise ValueError
+            if before<0:
+                raise ValueError
+            if before > after:
+                raise ValueError
+    if 0<=result<1:
+        print('E')
+    elif 99<result<=100:
+        print('F')
+    else:
+        print(f'{int(result)}%')
+
+while True:
+    try:
+        convert(input('Fraction: '))
+        break
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+
