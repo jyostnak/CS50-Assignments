af5f09091eb82a62a8dc1665e04f6dd9b006efa2
jyostnak <jyostnakondepi@gmail.com>
2026-05-18 10:41:18 +0000

    Mon, May 18, 2026, 4:11 PM +05:30

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -7,5 +7,11 @@ def test_Hello():
     assert value("Hello, I'm David") == 0
 
 def test_h():
-    assert value("")
+    assert value("hey") == 20
+
+def test_H():
+    assert value('Hey there!') == 20
+
+def test_rest():
+    assert value("What's up")
 
