ce10c523d0e8e9d26b391de8727a10bd7a8b6c3e
jyostnak <jyostnakondepi@gmail.com>
2026-05-18 10:30:45 +0000

    Mon, May 18, 2026, 4:00 PM +05:30

--- bank/bank.py
+++ bank/bank.py
@@ -1,11 +1,20 @@
-a=input("Greetings:")
-a = a.strip().lower()
-a=a.replace(',','')
-a=a.split()
-b="".join(a)
-if 'hello' in a:
-    print("$0")
-elif 'h' in b[0]:
-    print('$20')
-else:
-    print('$100')
+def main():
+    a=input("Greetings:")
+    greet = value(a)
+    print(greet)
+
+def value(greeting):
+    a = greeting.strip().lower()
+    a=a.replace(',','')
+    a=a.split()
+    b="".join(a)
+    if 'hello' in a:
+        return "$0"
+    elif 'h' in b[0]:
+        return '$20'
+    else:
+        return '$100'
+
+if __name__ == '__main__':
+    main()
+
