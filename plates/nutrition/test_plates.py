7dec4acb443696e4e6d9fb8104f4b12135e3f208
jyostnak <jyostnakondepi@gmail.com>
2026-05-18 11:03:20 +0000

    Mon, May 18, 2026, 4:33 PM +05:30

--- plates/nutrition/test_plates.py
+++ plates/nutrition/test_plates.py
@@ -2,3 +2,18 @@ from plates import is_valid
 
 def test_HELLO():
     assert is_valid("HELLO") == True
+
+def test_HELLOW():
+    assert is_valid("HELLO, WORLD") == False
+
+def test_GOODBYE():
+    assert is_valid("GOODBYE") == False
+
+def test_alphanum():
+    assert is_valid("CS50") == True
+
+def test_alphanum0():
+    assert is_valid("CS05") == False
+
+def test_num():
+    assert is_valid("50") == False
