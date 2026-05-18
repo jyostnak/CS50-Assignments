623ad4b20ae56a125eb34a3f0b8c2b5d22ff8035
jyostnak <jyostnakondepi@gmail.com>
2026-05-18 11:27:45 +0000

    Mon, May 18, 2026, 4:57 PM +05:30

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -3,5 +3,14 @@ from fuel import gauge
 
 def test_convert():
     assert convert('3/4') == (3/4)*100
-    assert convert()
+    assert convert('1/4') == (1/4)*100
+    assert convert('0/4') == 0
+    assert convert('4/0') == ZeroDivisionError
+
+def test_gauge():
+    assert gauge(0) == 'E'
+    assert gauge(0.7) == 'E'
+    assert gauge(56) == '56%'
+    assert gauge(98) == 'F'
+    assert gauge(100) == 'F'
 
