ecef15f8062fe24fd72de2b07066b325d09b846b
jyostnak <jyostnakondepi@gmail.com>
2026-05-21 15:20:08 +0000

    Thu, May 21, 2026, 8:50 PM +05:30

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,4 +1,15 @@
 from numb3rs import validate
 
-def test_validate():
-    assert 
+def test_validateryt():
+    assert validate('1.2.3.4') == True
+    assert validate('1.23.45.67') == True
+
+def test_validatesyntax():
+    assert validate('1234') == False
+    assert validate('1.234') == False
+    assert validate('1.2.34') == False
+    assert validate('12.34.') == False
+
+def test_validatenum():
+    assert validate('267.2.3.4') == False
+    assert validate('1.278.3.4') == False
