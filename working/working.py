5b10bcef2953bb7ee9fb34238abcb569cf1d118a
jyostnak <jyostnakondepi@gmail.com>
2026-05-22 12:04:57 +0000

    Fri, May 22, 2026, 5:34 PM +05:30

--- working/working.py
+++ working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    pattern = r"(\d):?(\d)?(AM|PM) to (\d):?(\d)?(AM|PM)"
+    pattern = r"(\d):?(\d)? (AM|PM) to (\d):?(\d)? (AM|PM)"
     match = re.search(pattern, s)
 
     if not match:
