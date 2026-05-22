fc2b5feb351ce59b0bee8728eb6a8c502bcfb2b4
jyostnak <jyostnakondepi@gmail.com>
2026-05-22 11:20:57 +0000

    Fri, May 22, 2026, 4:50 PM +05:30

--- working/working.py
+++ working/working.py
@@ -9,7 +9,10 @@ def main():
 def convert(s):
     pattern = r"(\d):?(\d)?(AM|PM) to (\d):?(\d)?(AM|PM)"
     if match := re.search(pattern, s):
-        
+        if int(match.group(1))>12 or int(match.group(4))>12:
+            raise ValueError
+        if match.group(2)>60 or int(match.group(5))>60:
+            raise ValueError
 
 
 
