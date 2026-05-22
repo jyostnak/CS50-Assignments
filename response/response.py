288e1ae1255953bd5a948f9e4880d0b3f9b87d23
jyostnak <jyostnakondepi@gmail.com>
2026-05-22 12:52:06 +0000

    Fri, May 22, 2026, 6:22 PM +05:30

--- response/response.py
+++ response/response.py
@@ -1,11 +1,11 @@
-import re
+
 import sys
 import validators
 
 email = input("What's your email address? ")
-pattern = r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
-if match := re.search(pattern, email):
-    if validators.email(email):
-            print("Valid")
-    else:
-            print("Invalid")
+
+
+if validators.email(email):
+        print("Valid")
+else:
+        print("Invalid")
