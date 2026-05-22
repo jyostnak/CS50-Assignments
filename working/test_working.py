201c99d492a74d425b1176ced099251b14801502
jyostnak <jyostnakondepi@gmail.com>
2026-05-22 11:58:55 +0000

    Fri, May 22, 2026, 5:28 PM +05:30

---
+++ working/test_working.py
@@ -0,0 +1,10 @@
+from working import convert
+
+def test_convertcorrect():
+    assert convert('9:00 AM to 5:00 PM') == '09:00 to 17:00'
+    assert convert('9 AM to 5 PM') == '09:00 to 17:00'
+    assert convert('9:30 AM to 5 PM') == '09:30 to 17:00'
+    assert convert('9:00 AM to 5:30 PM') == '09:00 to 17:30'
+
+def test_convertincor():
+    assert convert('9:65 AM to 5:00 PM')
