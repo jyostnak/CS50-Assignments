86848820c99acbe7956bdd95ede1eb9f8c822b93
jyostnak <jyostnakondepi@gmail.com>
2026-05-22 12:48:48 +0000

    Fri, May 22, 2026, 6:18 PM +05:30

--- response/response.py
+++ response/response.py
@@ -3,4 +3,8 @@ import sys
 import validators
 
 email = input("What's your email address? ")
-pattern = r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+
+if validators.email(email):
+        print("Valid")
+else:
+        print("Invalid")
