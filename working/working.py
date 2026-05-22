4ad08b0753f87ec1642bc1c2c6a01b539f6e5eb9
jyostnak <jyostnakondepi@gmail.com>
2026-05-22 11:42:35 +0000

    Fri, May 22, 2026, 5:12 PM +05:30

--- working/working.py
+++ working/working.py
@@ -9,21 +9,21 @@ def main():
 def convert(s):
     pattern = r"(\d):?(\d)?(AM|PM) to (\d):?(\d)?(AM|PM)"
     if match := re.search(pattern, s):
-        h1 = match.group(1)
-        m1 = match.group(2)
+        h1 = int(match.group(1))
+        m1 = int(match.group(2))
         p1 = match.group(3)
 
-        h2 = match.group(4)
-        m2 = match.group(5)
+        h2 = int(match.group(4))
+        m2 = int(match.group(5))
         p2 = match.group(6)
 
         if m1>60 or m2>60:
             raise ValueError
 
         if m1 is None:
-            m1 = '00'
+            m1 = 00
         if m2 is None:
-            m2 = '00'
+            m2 = 00
 
         if h1>12 or h2>12:
             raise ValueError
@@ -31,14 +31,14 @@ def convert(s):
         if p1 == 'PM':
             h1 = h1 + 12
         if p1 == 'AM':
-            if h1 == '12':
-                h1 = '00'
-                
+            if h1 == 12:
+                h1 = 00
+
         if p2 == 'PM':
             h2 = h2 + 12
-         if p2 == 'AM':
-            if h2 == '12':
-                h2 = '00'
+        if p2 == 'AM':
+            if h2 == 12:
+                h2 = 00
 
 
 
