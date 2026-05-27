bd38c264c3938854d07c7a0dc32cf23ef4ad695c
jyostnak <jyostnakondepi@gmail.com>
2026-05-27 11:10:08 +0000

    Wed, May 27, 2026, 4:40 PM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -12,8 +12,11 @@ def main():
     year, month, date_ = bday.split('-')
     today = date.today()
     curr_year = today.year
-    if int(month)
-    diff = curr_year - int(year)
+    if int(month) == today.month:
+        diff = curr_year - int(year)
+    else:
+        diff = curr_year - int(year) - 1
+
 
 
 
