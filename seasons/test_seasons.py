3241f79c74310638b1ce0a9ac3d6fa77b0f501fa
jyostnak <jyostnakondepi@gmail.com>
2026-05-28 09:06:12 +0000

    Thu, May 28, 2026, 2:36 PM +05:30

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -4,6 +4,9 @@ import pytest
 def test_convertryt():
     assert convert('2007-10-20') == 'Nine million, seven hundred eighty-four thousand, eight hundred minutes'
 
-def test_convert_():
-    assert convert('2007/10/20') == 'Invalid date'
-    assert convert('October 20, 2007') == 'Invalid date'
+def test_convert():
+    with pytest.raises(SystemExit):
+        convert("2007/10/20")
+
+    with pytest.raises(SystemExit):
+        convert("October 20, 2007")
