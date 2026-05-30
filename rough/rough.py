64e330819a393bba50b16b9229386020e97714c3
jyostnak <jyostnakondepi@gmail.com>
2026-05-30 16:57:56 +0000

    Sat, May 30, 2026, 10:27 PM +05:30

--- rough/rough.py
+++ rough/rough.py
@@ -8,72 +8,76 @@ import matplotlib.pyplot as plt
 import random
 
 def get_questions():
-    subject = int(input("Choose Subject:\n1. Computer Science\n2. Mathematics\n3. Science\n4. History\n5. Geography\n6. General Knowledge\n7. Politics\n8. Art\n9. Gadgets"))
-    subjects = {
-    1: 18,  # Computer Science
-    2: 19,  #Mathematics
-    3: 17,  # Science & Nature
-    4: 23,  # History
-    5: 22,  # Geography
-    6: 9,   # General Knowledge
-    7: 24,  # Politics
-    8: 25,  # Art
-    9: 30  # Science: Gadgets
-}
-    category = subjects[subject]
-    amount = input("How many questions? ")
-    response = requests.get(f"https://opentdb.com/api.php?amount={amount}&category={category}")
-    data = response.json()
-    questions = data['results']
-    score = 0
-    for q in questions:
-        print(html.unescape(q["question"]))
+    try:
+        subject = int(input("Choose Subject:\n1. Computer Science\n2. Mathematics\n3. Science\n4. History\n5. Geography\n6. General Knowledge\n7. Politics\n8. Art\n9. Gadgets"))
+        subjects = {
+        1: 18,  # Computer Science
+        2: 19,  #Mathematics
+        3: 17,  # Science & Nature
+        4: 23,  # History
+        5: 22,  # Geography
+        6: 9,   # General Knowledge
+        7: 24,  # Politics
+        8: 25,  # Art
+        9: 30  # Science: Gadgets
+    }
+        category = subjects[subject]
+        amount = input("How many questions? ")
+        response = requests.get(f"https://opentdb.com/api.php?amount={amount}&category={category}")
+        data = response.json()
+        questions = data['results']
+        score = 0
+        for q in questions:
+            print(html.unescape(q["question"]))
 
-        if q["type"] == 'multiple':
-            options = []
-            options.extend([html.unescape(x) for x in q["incorrect_answers"]])
-            options.append(html.unescape(q['correct_answer']))
-            random.shuffle(options)
-            for i, op in enumerate(options, start = 1):
-                print(f'{i}. {op}')
-            correct_option = options.index(q['correct_answer']) + 1
-            i = 0
-            while i <= 3:
-                try:
-                    user_ans = int(input('Answer: '))
-                    selected_option = options[user_ans - 1]
-                    if user_ans == correct_option:
-                        print("Correct answer!")
-                        score += 1
-                        break
-                    else:
-                        if i == 3:
-                            print(f"Correct answer: {q["correct_answer"]}")
-                        else:
-                            print("Oops.. Try again!")
-                except ValueError:
-                    print("Please enter valid option number.")
-                except IndexError:
-                    print("Please enter a valid option number.")
-
-                i += 1
-
-        if q["type"] == "boolean":
-                try:
-                    user_anstf = input("Answer(True/False): ").strip().capitalize()
-                    if user_anstf in ["True", "false"]:
-                        if user_anstf == (q["correct_answer"]):
+            if q["type"] == 'multiple':
+                options = []
+                options.extend([html.unescape(x) for x in q["incorrect_answers"]])
+                options.append(html.unescape(q['correct_answer']))
+                random.shuffle(options)
+                for i, op in enumerate(options, start = 1):
+                    print(f'{i}. {op}')
+                correct_option = options.index(q['correct_answer']) + 1
+                i = 0
+                while i <= 3:
+                    try:
+                        user_ans = int(input('Answer: '))
+                        selected_option = options[user_ans - 1]
+                        if user_ans == correct_option:
                             print("Correct answer!")
                             score += 1
                             break
                         else:
-                            print("Uh-ohh.. That was wrong!")
-                            print(f'Correct answer: {q["correct_answer"]}')
-                    else:
-                        raise ValueError
-                except ValueError:
-                    print("This is a True/False question.")
-                    print("Please enter only True or False.")
-    print(f"Score: {score}/{amount}")
+                            if i == 3:
+                                print(f"Correct answer: {q["correct_answer"]}")
+                            else:
+                                print("Oops.. Try again!")
+                    except ValueError:
+                        print("Please enter valid option number.")
+                    except IndexError:
+                        print("Please enter a valid option number.")
+
+                    i += 1
+
+            if q["type"] == "boolean":
+                    try:
+                        user_anstf = input("Answer(True/False): ").strip().capitalize()
+                        if user_anstf in ["True", "false"]:
+                            if user_anstf == (q["correct_answer"]):
+                                print("Correct answer!")
+                                score += 1
+                                break
+                            else:
+                                print("Uh-ohh.. That was wrong!")
+                                print(f'Correct answer: {q["correct_answer"]}')
+                        else:
+                            raise ValueError
+                    except ValueError:
+                        print("This is a True/False question.")
+                        print("Please enter only True or False.")
+        print(f"Score: {score}/{amount}")
+    except KeyError:
+        print("Invalid subject.")
+
 
 get_questions()
