eb8a06e998b92915f8d659ea3036e08f0ec050a7
jyostnak <jyostnakondepi@gmail.com>
2026-05-30 12:39:32 +0000

    Sat, May 30, 2026, 6:09 PM +05:30

--- rough/rough.py
+++ rough/rough.py
@@ -9,6 +9,7 @@ import random
 def get_questions():
     amount = input("How many questions? ")
     response = requests.get(f"https://opentdb.com/api.php?amount={amount}")
-    
-
+    data = response.json
+    questions = data['results']
+    print(questions[0]["question"])
 get_questions()
