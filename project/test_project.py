acb4d6b4d49b9d62eeb2b074dc0e403a273ebfb1
jyostnak <jyostnakondepi@gmail.com>
2026-06-03 14:28:14 +0000

    Wed, Jun 3, 2026, 7:58 PM +05:30

--- project/test_project.py
+++ project/test_project.py
@@ -1,2 +1,4 @@
-from project import calculate_percentage()
-
+from project import calculate_percentage
+from project import load_scores
+from project import save_scores
+from project import format_time
