45404e4b59dd001b5ce69b0570cfa2f7ffc0d23a
jyostnak <jyostnakondepi@gmail.com>
2026-06-03 14:34:02 +0000

    Wed, Jun 3, 2026, 8:04 PM +05:30

--- project/test_project.py
+++ project/test_project.py
@@ -2,4 +2,9 @@ from project import calculate_percentage, load_scores, save_scores, format_time
 import pytest
 
 def test_calculate_percentage():
-    assert calculate_percentage()
+    assert calculate_percentage(4, 5) == 4/5*100
+    assert caluculate_percentage(6, 6) == 100
+    assert calculate_percentage(8, 9) == 8/9*100
+
+
+def test_
