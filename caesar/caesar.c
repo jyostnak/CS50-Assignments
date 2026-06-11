66754bfd372f7e3a3257d20f90d7ab60f36894a4
jyostnak <jyostnakondepi@gmail.com>
2026-06-11 09:12:05 +0000

    Thu, Jun 11, 2026, 2:42 PM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -13,6 +13,8 @@ int main(int argc, string argv[])
         return 1;
     }
 
+    printf("ciphertext: ");
+
     for (int i = 0; i < strlen(argv[1]); i++)
     {
         if (!isdigit(argv[1][i]))
@@ -41,4 +43,5 @@ int main(int argc, string argv[])
             printf("%c", text[i]);
         }
     }
+    printf("\n");
 }
