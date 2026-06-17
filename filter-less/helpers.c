4d77e26e8d50e700aecc1a57942dbbdb2cd7d145
jyostnak <jyostnakondepi@gmail.com>
2026-06-17 16:06:42 +0000

    Wed, Jun 17, 2026, 9:36 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -9,8 +9,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Take average of red, green, and blue
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+            int avg = round((red + green + blue) / 3)
 
             // Update pixel values
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
         }
     }
     return;
