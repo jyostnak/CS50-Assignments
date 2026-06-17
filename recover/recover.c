f557a17a4cf169ebd4dd7611fe7a5ebe5f8bc9b5
jyostnak <jyostnakondepi@gmail.com>
2026-06-17 15:12:53 +0000

    Wed, Jun 17, 2026, 8:42 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -28,14 +28,14 @@ int main(int argc, char *argv[])
         (buffer[3] & 0xf0) == 0xe0)
         {
             // If we're already writing a JPEG,
+            // close it before starting a new one
             if (img != NULL)
             {
             fclose(img);
             }
-            // close it before starting a new one
 
             // Create filename: 000.jpg, 001.jpg, ...
-
+            
             // Open the new JPEG file
 
             // Increase jpg_count
